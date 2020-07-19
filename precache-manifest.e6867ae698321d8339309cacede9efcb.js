85e6a14f1de2b72a567c6203ce2c0bd52aa96918
Reid B <reidburnsy19@hotmail.com>
2020-07-19 19:43:17 -0400

    Updates

---
+++ precache-manifest.e6867ae698321d8339309cacede9efcb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d17764f221fc3e60bd5d681deb9a57f",
+    "url": "/Updated_Portfolio/index.html"
+  },
+  {
+    "revision": "34263db2646964a90404",
+    "url": "/Updated_Portfolio/static/css/main.a45f678e.chunk.css"
+  },
+  {
+    "revision": "d698a2bbd60a0cd0fb9d",
+    "url": "/Updated_Portfolio/static/js/2.a6fe42df.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/Updated_Portfolio/static/js/2.a6fe42df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34263db2646964a90404",
+    "url": "/Updated_Portfolio/static/js/main.fbcdf081.chunk.js"
+  },
+  {
+    "revision": "38f49ad4aa8d66dd2619",
+    "url": "/Updated_Portfolio/static/js/runtime-main.ed608aed.js"
+  },
+  {
+    "revision": "9851f2fd3a4587e72011af369875fa31",
+    "url": "/Updated_Portfolio/static/media/LIRIbot.9851f2fd.png"
+  },
+  {
+    "revision": "0ee5a8754bed3b850c8c201c27b91f29",
+    "url": "/Updated_Portfolio/static/media/me.0ee5a875.jpg"
+  },
+  {
+    "revision": "bfce7a882e75f4256bf05e6f8e9b90ba",
+    "url": "/Updated_Portfolio/static/media/project1.bfce7a88.png"
+  },
+  {
+    "revision": "df2a884553f3afe74cb9721d0e0d05b5",
+    "url": "/Updated_Portfolio/static/media/resume.df2a8845.pdf"
+  },
+  {
+    "revision": "0824c898ddab010f810273c686d5bb6d",
+    "url": "/Updated_Portfolio/static/media/train.0824c898.png"
+  }
+]);
